d6a053e04cff84ac3d01f9ef21ba6f7d03be098e
Artur Hamann Ronconi <arturhronconi@gmail.com>
2021-05-10 17:06:39 -0300

    Remake categories routes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,10 +1,25 @@
 import { Router } from 'express'
 
-import categoryController from '../controllers/CategoryController'
+import categoryRepository from '../repositories/CategoryRepository'
 
 const categoriesRoutes = Router()
 
 categoriesRoutes.route('/categories')
-  .post(categoryController.create)
+  .post((req, res) => {
+    const { name, description } = req.body
+
+    const categoryAlreadyExists = categoryRepository.findByName(name)
+    if(categoryAlreadyExists)
+      return res.status(400).json({ message: 'Category already exists' })
+
+    categoryRepository.create({ name, description })
+
+    return res.status(201).end()
+  })
+  .get((req, res) => {
+    const categories = categoryRepository.list()
+
+    return res.status(200).json(categories)
+  })
 
 export default categoriesRoutes
