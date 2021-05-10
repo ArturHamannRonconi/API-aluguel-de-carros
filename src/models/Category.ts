0dd04a2355baa33a50d7ee4c5a5819164603e663
Artur Hamann Ronconi <arturhronconi@gmail.com>
2021-05-10 17:06:04 -0300

    Remake interface to class Category

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,9 +1,16 @@
-interface Category
+import { v4 as generateUUID } from 'uuid'
+
+class Category
 {
   name: string
   description: string
-  id: string
+  id?: string
   created_at: Date
+
+  constructor()
+  {
+    if(!this.id) this.id = generateUUID()
+  }
 }
 
 export default Category
