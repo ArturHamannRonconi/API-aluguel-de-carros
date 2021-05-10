d361b2760c533d906eabefcb7c8c47179bb9e1a2
Artur Hamann Ronconi <arturhronconi@gmail.com>
2021-05-10 15:26:57 -0300

    create categories routes

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express'
+
+const categoriesRoutes = Router()
+const categories = []
+
+
+categoriesRoutes.post('/categories', (req, res) => {
+  const { name, description } = req.body
+
+  const newCategory = {
+    name,
+    description
+  }
+
+  categories.push(newCategory)
+  
+  return res.status(201).json(newCategory) 
+})
+
+export default categoriesRoutes
