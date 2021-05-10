a5255a54fc28d74a4f0bcf493b30f56fde8e2afe
Artur Hamann Ronconi <arturhronconi@gmail.com>
2021-05-10 17:05:18 -0300

    Remove Services, Controllers and database

--- src/database/Database.ts
+++
@@ -1,8 +0,0 @@
-import Category from '../models/Category'
-
-interface Database
-{
-  categories: Category[]
-}
-
-export default Database
--- src/database/index.ts
+++
@@ -1,9 +0,0 @@
-import Database from './Database'
-
-const database: Database = {
-  categories: [],
-
-}
-
-export default database
-export const { categories } = database
