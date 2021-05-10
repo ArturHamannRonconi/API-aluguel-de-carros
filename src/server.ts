6b3df32c6b7eb5693e670f71ad0403d58f2d0e9f
Artur Hamann Ronconi <arturhronconi@gmail.com>
2021-05-10 15:27:32 -0300

    att server.ts with routes

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
-import './config.ts'
 import express from 'express'
+
+import './config.ts'
+import routes from './routes'
 
 const app = express()
 
-app.get('/', (req, res) => res.json({ message: 'Hello World!' }))
+app.use(express.json())
+app.use(routes)
 
 app.listen(process.env.PORT, () => console.log('Server is running...'))
