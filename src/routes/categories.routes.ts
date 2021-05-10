736b68789daf7c20f63c64dcc7e64d5230f480f8
Artur Hamann Ronconi <arturhronconi@gmail.com>
2021-05-10 16:09:51 -0300

    alter categories routes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,20 +1,10 @@
 import { Router } from 'express'
 
+import categoryController from '../controllers/CategoryController'
+
 const categoriesRoutes = Router()
-const categories = []
 
-
-categoriesRoutes.post('/categories', (req, res) => {
-  const { name, description } = req.body
-
-  const newCategory = {
-    name,
-    description
-  }
-
-  categories.push(newCategory)
-  
-  return res.status(201).json(newCategory) 
-})
+categoriesRoutes.route('/categories')
+  .post(categoryController.create)
 
 export default categoriesRoutes
