74e00026c370a168de624d6f0f0ce60648a99050
ArturHamannRonconi <arturhronconi@gmail.com>
2021-05-31 10:25:36 -0300

    Recreate cars migration

--- src/shared/infra/database/typeorm/migrations/1622460485876-CreateCars.ts
+++ src/shared/infra/database/typeorm/migrations/1622460485876-CreateCars.ts
@@ -1,14 +1,14 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm'
 
-export class CreateCars1622460485876 implements MigrationInterface {
-
+export class CreateCars1622460485876 implements MigrationInterface
+{
   public async up(queryRunner: QueryRunner): Promise<void>
   {
     await queryRunner.createTable(
       new Table({
         name: 'cars',
         columns: [
-          { name: 'id', type: 'uuid', isPrimary: true, generationStrategy: 'uuid', default: 'uuid_generate_v4()' },
+          { name: 'id', type: 'uuid', isPrimary: true, default: 'gen_random_uuid()' },
           { name: 'name', type: 'varchar' },
           { name: 'description', type: 'varchar' },
           { name: 'daily_rate', type: 'numeric' },
@@ -25,17 +25,16 @@ export class CreateCars1622460485876 implements MigrationInterface {
             columnNames: ['category'],
             referencedTableName: 'categories',
             referencedColumnNames: ['id'],
-            onDelete: 'SET_NULL',
-            onUpdate: 'SET_NULL'
+            onDelete: 'SET NULL',
+            onUpdate: 'SET NULL'
           }
         ]
       })
     )
   }
-
+    
   public async down(queryRunner: QueryRunner): Promise<void>
   {
     await queryRunner.dropTable('cars')
   }
-
 }
