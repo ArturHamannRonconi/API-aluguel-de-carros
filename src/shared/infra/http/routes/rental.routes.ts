3a59630a5f69268d733c60b5432547ad6577f21a
ArturHamannRonconi <arturhronconi@gmail.com>
2021-06-11 12:03:16 -0300

    add images into gitignore

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -3,8 +3,15 @@ import { Router } from 'express'
 import authenticationHandler from '../middlewares/AuthenticationHandler'
 import createRentalController from '@rentals/controllers/rentalControllers/CreateRentalController'
 import devolutionREntalController from '@rentals/controllers/rentalControllers/DevolutionRentalController'
+import listRentalByUserController from '@rentals/controllers/rentalControllers/ListRentalByUserConroller'
 
 const rentalRoutes = Router()
+
+rentalRoutes.route('/')
+  .get(
+    authenticationHandler.exec,
+    listRentalByUserController.handle
+  )
 
 rentalRoutes.route('/devolution')
   .post(
