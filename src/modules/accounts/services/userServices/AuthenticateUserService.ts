9c0a4cfb306890fe3da45e2ef8344a5a2dd74258
ArturHamannRonconi <arturhronconi@gmail.com>
2021-06-13 12:23:13 -0300

    modified user Authentication service

--- src/modules/accounts/services/userServices/AuthenticateUserService.ts
+++ src/modules/accounts/services/userServices/AuthenticateUserService.ts
@@ -1,19 +1,24 @@
 import { compare } from 'bcrypt'
 import { inject, injectable } from 'tsyringe'
-import { SignOptions, Secret, sign } from 'jsonwebtoken'
+import { SignOptions, sign } from 'jsonwebtoken'
 
 import IUserRepository from '@accounts/repositories/interfaces/IUserRepository'
 import UserAccount from '@myTypes/UserAccount'
 import Payload from '@myTypes/Payload'
 import AuthenticateResponse from '@myTypes/AuthenticateReponse'
 import AppError from '@shared/errors/AppError'
+import IUserTokenRepository from '@accounts/repositories/interfaces/IUserTokenRepository'
+import Auth from '@config/Auth'
+import DayjsDateProvider from '@shared/container/providers/DateProvider/implementations/DayjsDateProvider'
 
 @injectable()
 class AuthenticateUserService
 {
   constructor(
     @inject('UserRepository')
-    private userRepository: IUserRepository
+    private userRepository: IUserRepository,
+    @inject('UserTokenRepository')
+    private userTokenRepository: IUserTokenRepository
   ) {  }
 
   public async execute({ email, username, password }: UserAccount): Promise<AuthenticateResponse>
@@ -24,11 +29,27 @@ class AuthenticateUserService
     if(!user || !passwordMatch)
       throw new AppError('Email, username or password incorrect')
 
-    const payload: Payload = {  }
-    const privateKey: Secret = '7a08a0940a9343e60c812a9a70ea65cb'
-    const options: SignOptions = { subject: user.id, expiresIn: '1d' }
+    const payloadToken: Payload = {  }
+    const payloadRefreshToken: Payload = { email }
+    const optionsRefreshToken: SignOptions = {
+      subject: user.id,
+      expiresIn: Auth.EXPIRES_REFRESH_TOKEN
+    }
+    const optionsToken: SignOptions = {
+      subject: user.id,
+      expiresIn: Auth.EXPIRES_TOKEN
+    }
 
-    return { token: sign(payload, privateKey, options) }
+    const token = sign(payloadToken, Auth.PRIVATE_KEY_TOKEN, optionsToken)
+    const refresh_token = sign(payloadRefreshToken, Auth.PRIVATE_KEY_REFRESH_TOKEN, optionsRefreshToken) 
+
+    await this.userTokenRepository.create({
+      user_id: user.id,
+      expires_date: DayjsDateProvider.addDays(Auth.EXPIRES_REFRESH_TOKEN_DAYS),
+      refresh_token
+    })
+
+    return { token, refresh_token }
   }
 }
 
--- src/shared/infra/http/middlewares/AuthenticationHandler.ts
+++ src/shared/infra/http/middlewares/AuthenticationHandler.ts
@@ -1,9 +1,10 @@
 import { Request, Response, NextFunction } from 'express'
-import { verify, Secret } from 'jsonwebtoken'
+import { verify } from 'jsonwebtoken'
 
 import PayloadDecoded from '@myTypes/PayloadDecoded'
 import UserRepository from '@accounts/infra/typeorm/repositories/UserRepository'
 import AppError from '@shared/errors/AppError'
+import Auth from '@config/Auth'
 
 class AuthenticationHandler
 {
@@ -14,9 +15,8 @@ class AuthenticationHandler
     if(!authHeader) throw new AppError('Token missing', 401)
 
     const token = authHeader.split(' ').pop()
-    const privateKey: Secret = '7a08a0940a9343e60c812a9a70ea65cb'
 
-    const payload = verify(token, privateKey) as PayloadDecoded
+    const payload = verify(token, Auth.PRIVATE_KEY_TOKEN) as PayloadDecoded
     if(!payload) throw new AppError('Invalid Token', 401)
   
     const userRepository = new UserRepository() 
